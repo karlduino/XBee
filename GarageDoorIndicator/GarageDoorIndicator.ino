de381f8491487117a55c577e7b9bf78f92cb5512
Karlduino <karlduino@gmail.com>
2012-08-02 18:01:04 -0500

    Change arduino pins for indicator lights
      - Also, changed the order of the LED flashes
    
      - Also, XBee remote now gives 1 = open and 0 = closed

--- GarageDoorIndicator/GarageDoorIndicator.ino
+++ GarageDoorIndicator/GarageDoorIndicator.ino
@@ -7,10 +7,10 @@
  *         sample rate set at 2000 ms (ATIR7D0)
  *
  * XBee coordinator attached to arduino
- *         arduino digital pin  4: small door shut
- *         arduino digital pin  6: small door open
- *         arduino digital pin  8: large door shut
- *         arduino digital pin 10: large door open
+ *         arduino digital pin 10: small door open
+ *         arduino digital pin  8: small door shut
+ *         arduino digital pin  6: large door open
+ *         arduino digital pin  4: large door shut
  *
  * xbee-arduino library from https://code.google.com/p/xbee-arduino/
  **/
@@ -20,10 +20,10 @@ XBee xbee = XBee();
 ZBRxIoSampleResponse ioSample = ZBRxIoSampleResponse();
 
 const int errorLED =    13;
-const int smClosedLED =  4;
-const int smOpenLED =    6;
-const int lgClosedLED =  8;
-const int lgOpenLED =   10;
+const int smOpenLED =   10;
+const int smClosedLED =  8;
+const int lgOpenLED =    6;
+const int lgClosedLED =  4;
 
 int lgClosed=0, smClosed=0;
 unsigned long lastRead = 0; 
@@ -39,10 +39,10 @@ void setup() {
 
   // show that the LEDs are working
   flashLED(errorLED, 10, 50);
-  flashLED(smClosedLED, 10, 50);
   flashLED(smOpenLED, 10, 50);
-  flashLED(lgClosedLED, 10, 50);
+  flashLED(smClosedLED, 10, 50);
   flashLED(lgOpenLED, 10, 50);
+  flashLED(lgClosedLED, 10, 50);
 }
 
 void loop() {
@@ -54,8 +54,8 @@ void loop() {
     if (xbee.getResponse().getApiId() == ZB_IO_SAMPLE_RESPONSE) {
       xbee.getResponse().getZBRxIoSampleResponse(ioSample);
 
-      smClosed = ioSample.isDigitalOn(0);
-      lgClosed = ioSample.isDigitalOn(1);
+      smClosed = 1 - ioSample.isDigitalOn(0);
+      lgClosed = 1 - ioSample.isDigitalOn(1);
       lastRead = millis();
 
       if(lastRead + errorGap < lastRead) lastRead = 0; // roll over?
@@ -72,10 +72,10 @@ void loop() {
 
     // error signal
     flashLED(errorLED, 1, 100);
-    flashLED(smClosedLED, 1, 100);
     flashLED(smOpenLED, 1, 100);
-    flashLED(lgClosedLED, 1, 100);
+    flashLED(smClosedLED, 1, 100);
     flashLED(lgOpenLED, 1, 100);
+    flashLED(lgClosedLED, 1, 100);
   }
   else {
 
